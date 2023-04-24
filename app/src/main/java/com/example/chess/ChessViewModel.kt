aa31221497570cc048165584bf34eaf442d5beb3
abvickers <abvickers@vt.edu>
2023-04-24 14:49:05 -0400

    Added functionality to viewmodel

--- app/src/main/java/com/example/chess/ChessViewModel.kt
+++ app/src/main/java/com/example/chess/ChessViewModel.kt
@@ -6,8 +6,8 @@ class ChessViewModel : ViewModel() {
 
     private var timeSetting: Int? = null
     private var theme: String? = null
-    private var moveCount1: Int? = null
-    private var moveCount2: Int? = null
+    private var moveCount1: Int = 0
+    private var moveCount2: Int = 0
     private var moveList1: MutableList<String?> = mutableListOf<String?>()
     private var moveList2: MutableList<String?> = mutableListOf<String?>()
 
@@ -19,6 +19,28 @@ class ChessViewModel : ViewModel() {
         return timeSetting
     }
 
+    fun setTheme(newTheme: String) {
+        theme = newTheme
+    }
 
+    fun getTheme(): String? {
+        return theme
+    }
+
+    fun addMove(player: Int, move: String) {
+        if (player == 1){
+            moveList1.add(move)
+        } else {
+            moveList2.add(move)
+        }
+    }
+
+    fun setMoveCount(player: Int) {
+        if (player == 1){
+            moveCount1++
+        } else {
+            moveCount2++
+        }
+    }
 
 }
